917805c1ddadd734082a77b0c90f265debda46d7
Mikita Slabysh <slabysh2015@gmail.com>
2021-06-05 23:35:15 -0500

    Search for countries

---
+++ script.js
@@ -0,0 +1,91 @@
+var map, searchManager;
+
+function GetMap() {
+    map = new Microsoft.Maps.Map('#myMap', {
+        credentials: 'AuV6Kc6hF3yFNL_DXFTDGuSu9DCdIK8zYF208z0eNdqbXtt87UHslIKJ70900Wbj'
+    });
+
+    //Load the spatial math module
+    Microsoft.Maps.loadModule("Microsoft.Maps.SpatialMath", function () {
+        //Request the user's location
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var loc = new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude);
+
+            //Create an accuracy circle
+            var path = Microsoft.Maps.SpatialMath.getRegularPolygon(loc, position.coords.accuracy, 36, Microsoft.Maps.SpatialMath.Meters);
+            var poly = new Microsoft.Maps.Polygon(path);
+            map.entities.push(poly);
+
+            //Add a pushpin at the user's location.
+            var pin = new Microsoft.Maps.Pushpin(loc);
+            map.entities.push(pin);
+
+            //Center the map on the user's location.
+            map.setView({ center: loc, zoom: 17 });
+        });
+    });
+}
+
+function Search() {
+    if (!searchManager) {
+        //Create an instance of the search manager and perform the search.
+        Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+            searchManager = new Microsoft.Maps.Search.SearchManager(map);
+            Search()
+        });
+    } else {
+        //Remove any previous results from the map.
+        map.entities.clear();
+
+        //Get the users query and geocode it.
+        var query = document.getElementById('searchTbx').value;
+        geocodeQuery(query);
+    }
+}
+
+function geocodeQuery(query) {
+    var searchRequest = {
+        where: query,
+        callback: function (r) {
+            if (r && r.results && r.results.length > 0) {
+                var pin, pins = [], locs = [], output = 'Results:<br/>';
+
+                for (var i = 0; i < r.results.length; i++) {
+                    //Create a pushpin for each result. 
+                    pin = new Microsoft.Maps.Pushpin(r.results[i].location, {
+                        text: i + ''
+                    });
+                    pins.push(pin);
+                    locs.push(r.results[i].location);
+
+                    output += i + ') ' + r.results[i].name + '<br/>';
+                }
+
+                //Add the pins to the map
+                map.entities.push(pins);
+
+                //Display list of results
+                document.getElementById('output').innerHTML = output;
+
+                //Determine a bounding box to best view the results.
+                var bounds;
+
+                if (r.results.length == 1) {
+                    bounds = r.results[0].bestView;
+                } else {
+                    //Use the locations from the results to calculate a bounding box.
+                    bounds = Microsoft.Maps.LocationRect.fromLocations(locs);
+                }
+
+                map.setView({ bounds: bounds });
+            }
+        },
+        errorCallback: function (e) {
+            //If there is an error, alert the user about it.
+            alert("No results found.");
+        }
+    };
+
+    //Make the geocode request.
+    searchManager.geocode(searchRequest);
+}
