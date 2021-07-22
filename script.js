28bd6ceaf6d1b16de6e49c4b8795f6d473427842
Mikita Slabysh <slabysh2015@gmail.com>
2021-07-22 01:06:24 -0400

    More search results

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var map, searchManager;
 var BingMapsKey = 'AuV6Kc6hF3yFNL_DXFTDGuSu9DCdIK8zYF208z0eNdqbXtt87UHslIKJ70900Wbj';
-
+let data = ""
+let userLat , userLong, updatedLat, updatedLong
 function GetMap() {
     map = new Microsoft.Maps.Map('#myMap', {
         credentials: BingMapsKey
@@ -11,7 +12,11 @@ function GetMap() {
         //Request the user's location
         navigator.geolocation.getCurrentPosition(function (position) {
             var loc = new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude);
-
+            userLat = position.coords.latitude
+            userLong = position.coords.longitude
+            updatedLat = userLat + 0.05
+            updatedLong = userLong + 0.05
+            geocode()
             //Create an accuracy circle
             var path = Microsoft.Maps.SpatialMath.getRegularPolygon(loc, position.coords.accuracy, 36, Microsoft.Maps.SpatialMath.Meters);
             var poly = new Microsoft.Maps.Polygon(path);
@@ -27,12 +32,18 @@ function GetMap() {
     });
 }
 
+
+
 function geocode() {
     var query = document.getElementById('input').value;
+    for (let x = 0; x < 10; x++){
+        const longlat = [userLat, userLong, updatedLat, updatedLong]
+        var geocodeRequest = "http://dev.virtualearth.net/REST/v1/LocalSearch/?query=" + encodeURIComponent(query) + "&userMapView=" + encodeURIComponent(longlat) + "&maxResults=25&jsonp=GeocodeCallback&key=" + BingMapsKey;
 
-    var geocodeRequest = "http://dev.virtualearth.net/REST/v1/LocalSearch?query=" + encodeURIComponent(query) + "&jsonp=GeocodeCallback&key=" + BingMapsKey;
-
-    CallRestService(geocodeRequest, GeocodeCallback);
+        CallRestService(geocodeRequest, GeocodeCallback);
+        updatedLat += 0.05
+        updatedLong += 0.05
+    }
 }
 
 function GeocodeCallback(response) {
@@ -45,18 +56,43 @@ function GeocodeCallback(response) {
 
         var results = response.resourceSets[0].resources;
 
-        var html = ['<table><tr><td>Name</td><td>Latitude</td><td>Longitude</td></tr>'];
+        var html = ['<table><tr><td>Name</td><td>Phone</td><td>Website</td></tr>'];
 
         for (var i = 0; i < results.length; i++) {
-            html.push('<tr><td>', results[i].name, '</td><td>', results[i].point.coordinates[0], '</td><td>', results[i].point.coordinates[1], '</td></tr>');
+            html.push('<tr><td>', results[i].name, '</td><td>', results[i].PhoneNumber, '</td><td>', results[i].Website, '</td></tr>');
+            data += results[i].PhoneNumber + "\n"
+            data += results[i].name + "\n"//Save Data into file
+            data += "\n"
         }
 
         html.push('</table>');
 
-        output.innerHTML = html.join('');
+        output.innerHTML += html.join('');
+
     } else {
         output.innerHTML = "No results found.";
     }
+}
+
+let WriteToFile = () => {
+
+    // Convert the text to BLOB.
+    const textToBLOB = new Blob([data], { type: 'text/plain' });
+    const sFileName = 'formData.txt';	   // The file to save the data.
+
+    let newLink = document.createElement("a");
+    newLink.download = sFileName;
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    }
+    else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click(); 
 }
 
 function CallRestService(request) {
@@ -65,67 +101,3 @@ function CallRestService(request) {
     script.setAttribute("src", request);
     document.body.appendChild(script);
 }
-
-/*function Search() {
-    if (!searchManager) {
-        //Create an instance of the search manager and perform the search.
-        Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
-            searchManager = new Microsoft.Maps.Search.SearchManager(map);
-            Search()
-        });
-    } else {
-        //Remove any previous results from the map.
-        map.entities.clear();
-
-        //Get the users query and geocode it.
-        var query = document.getElementById('searchTbx').value;
-        geocodeQuery(query);
-    }
-}
-
-function geocodeQuery(query) {
-    var searchRequest = {
-        where: query,
-        callback: function (r) {
-            if (r && r.results && r.results.length > 0) {
-                var pin, pins = [], locs = [], output = 'Results:<br/>';
-
-                for (var i = 0; i < r.results.length; i++) {
-                    //Create a pushpin for each result. 
-                    pin = new Microsoft.Maps.Pushpin(r.results[i].location, {
-                        text: i + ''
-                    });
-                    pins.push(pin);
-                    locs.push(r.results[i].location);
-
-                    output += i + ') ' + r.results[i].name + '<br/>';
-                }
-
-                //Add the pins to the map
-                map.entities.push(pins);
-
-                //Display list of results
-                document.getElementById('result').innerHTML = output;
-
-                //Determine a bounding box to best view the results.
-                var bounds;
-
-                if (r.results.length == 1) {
-                    bounds = r.results[0].bestView;
-                } else {
-                    //Use the locations from the results to calculate a bounding box.
-                    bounds = Microsoft.Maps.LocationRect.fromLocations(locs);
-                }
-
-                map.setView({ bounds: bounds });
-            }
-        },
-        errorCallback: function (e) {
-            //If there is an error, alert the user about it.
-            alert("No results found.");
-        }
-    };
-
-    //Make the geocode request.
-    searchManager.geocode(searchRequest);
-}*/
